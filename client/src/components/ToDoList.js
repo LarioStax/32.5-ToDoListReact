87b1078faf25b9fe9579639068c02739c95fc2da
StaxGit <lario.free@gmail.com>
2020-05-26 13:12:33 +0200

    Fetch todos from db, put them in state, handle errors

--- client/src/components/ToDoList.js
+++ client/src/components/ToDoList.js
@@ -1,6 +1,34 @@
 import React, {Component} from "react";
 
+const APIURL = "/api/todos"
+
 class ToDoList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: []
+    }
+    this.loadTodos = this.loadTodos.bind(this);
+  }
+
+  componentDidMount() {
+    this.loadTodos();
+  }
+
+  loadTodos() {
+    fetch(APIURL)
+    //fetch doesn't throw error with 4xx and 5xx responses
+    .then(response => {
+      if (response.status >= 200 && response.status <= 299) {
+        return response.json();
+      } else {
+        throw Error(response.statusText);
+      }
+    })
+    .then(todos => this.setState({todos}))
+    .catch(error => console.log(error));
+  }
+
   render() {
     return(
       <h1>ToDo List Component!</h1>
@@ -8,4 +36,4 @@ class ToDoList extends Component {
   }
 }
 
-export default ToDoList
+export default ToDoList;
