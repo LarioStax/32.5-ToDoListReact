0f383a5b78db4b7102fc191dbfa001bc05d361b0
StaxGit <lario.free@gmail.com>
2020-05-26 07:58:03 +0200

    Setup ToDoList component

---
+++ client/src/components/ToDoList.js
@@ -0,0 +1,11 @@
+import React, {Component} from "react";
+
+class ToDoList extends Component {
+  render() {
+    return(
+      <h1>ToDo List Component!</h1>
+    )
+  }
+}
+
+export default ToDoList
